ab2865472b34188e501696c7a0916e21f58392c3
JulioOLV <julioccolv@gmail.com>
2023-01-02 20:39:55 -0300

    feat: adiciona estruturação da aplicação

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "../src/infra/components/Header";
+
+function Home() {
+    return (
+        <div>
+            <Head />
+            <h1>Home</h1>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/infra/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import NextHead from 'next/head'
+
+function Head() {
+    return (
+        <div>
+            <NextHead>
+                <title>JulioOLV</title>
+            </NextHead>
+        </div>
+    )
+}
+
+export default Head
